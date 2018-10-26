0c1fc19d498fa11acbcd4f17d35fb8358ee1ab59
Thomas <t.dvornik@salesforce.com>
2018-10-26 14:26:08 -0600

    fix: remove shelljs from preinstall
    affects: @salesforce/dev-config, @salesforce/dev-scripts, @salesforce/kit, @salesforce/ts-types

---
+++ packages/sfdx-dev-scripts/bin/sfdx-husky-pre-commit.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+/*
+ * Copyright (c) 2018, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+const shell = require("shelljs");
+
+shell.set("-e");
+shell.set("+v");
+
+const packageRoot = require("../utils/package-root");
+
+shell.exec("pretty-quick --staged", {
+  cwd: packageRoot
+});
+
+// TODO targets not added yet
+// shell.exec("yarn docs", {
+//   cwd: packageRoot
+// });
+
+// shell.exec("git add **/docs", {
+//   cwd: packageRoot
+// });
