6cb15a9cbc082e4be6f0197c303a8c0c0a9edccc
Thomas <t.dvornik@salesforce.com>
2018-10-30 12:40:32 -0600

    feat: control devdependencies
    affects: @salesforce/dev-config, @salesforce/dev-scripts, @salesforce/kit, @salesforce/ts-types

--- packages/sfdx-dev-scripts/utils/write-dependencies.js
+++ packages/sfdx-dev-scripts/utils/write-dependencies.js
@@ -17,11 +17,41 @@ module.exports = (projectPath, inLernaProject) => {
   const dependencies = pjson.get('devDependencies');
 
   const added = [];
+  const removed = [];
 
-  if (!inLernaProject && config.list('husky').length > 0) {
-    added.push('husky => ^1');
-    dependencies['husky'] = '^1';
+  const remove = name => {
+    if (dependencies[name]) {
+      delete dependencies[name];
+      removed.push(name);
+    }
+  };
+
+  if (config.list('husky').length > 0) {
+    if (!inLernaProject) {
+      added.push('husky => ^1');
+      dependencies['husky'] = '^1';
+    } else {
+      remove('husky');
+    }
   }
+
+  // Included by dev-scripts
+  remove('nyc');
+  remove('typescript');
+  remove('source-map-support');
+  remove('tslint');
+  remove('xunit-file');
+  remove('sinon');
+  remove('mocha');
+  remove('chai');
+  remove('@types/chai');
+  remove('@types/mocha');
+  remove('@types/node');
+  remove('@types/sinon');
+  remove('typedoc');
+  remove('typedoc-plugin-external-module-name');
+  remove('prettier');
+  remove('pretty-quick');
 
   if (added.length > 0) {
     console.log(`added\n\t${added.join('\n\t')}`);
