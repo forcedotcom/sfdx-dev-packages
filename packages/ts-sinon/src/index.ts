981ba4e5ba3b17acead4f6276ecf4c537b7fa0c8
Bob Bergman <rbergman@salesforce.com>
2018-11-06 16:01:26 -0700

    fix: minor fn usage updates

--- packages/ts-sinon/src/index.ts
+++ packages/ts-sinon/src/index.ts
@@ -7,8 +7,7 @@
 
 import { SinonSandbox, SinonSpy, SinonStub } from 'sinon';
 
-import { AnyFunction, Dictionary } from '@salesforce/ts-types';
-import { isFunction } from 'util';
+import { AnyFunction, Dictionary, isFunction } from '@salesforce/ts-types';
 
 /**
  * A convenience for a `Dictionary` of `any` values.
@@ -32,15 +31,17 @@ export type Stub<V extends OpenFunction> = V & SinonStub;
  * Any type `T` whose function property values have been stubbed as {@link Stub}.
  */
 export type StubbedType<T extends object> = {
-    // tslint:disable-next-line:no-any
-    [K in keyof T]: T[K] extends OpenFunction ? Stub<T[K]> : T[K]
+  // tslint:disable-next-line:no-any
+  [K in keyof T]: T[K] extends OpenFunction ? Stub<T[K]> : T[K]
 };
 
 /**
  * Any {@link StubbedType} who is also a callable TypeScript function type.
  */
 // tslint:disable-next-line:no-any
-export type StubbedCallableType<T extends object> = Stub<StubbedType<T> & OpenFunction>;
+export type StubbedCallableType<T extends object> = Stub<
+  StubbedType<T> & OpenFunction
+>;
 
 /**
  * Provides the ability to stub methods on object instances and prototypes. More it specifically provides a mechanism
@@ -49,9 +50,13 @@ export type StubbedCallableType<T extends object> = Stub<StubbedType<T> & OpenFu
  * @param target The target object of the stubbing operation.
  * @param method The method name of the stub.
  */
-export function stubMethod<T extends object>(sandbox: SinonSandbox, target: T, method: string): SinonStub {
-    // force method to keyof T to allow stubbing private, protected, and methods otherwise not exposed in typings
-    return sandbox.stub(target, method as keyof T);
+export function stubMethod<T extends object>(
+  sandbox: SinonSandbox,
+  target: T,
+  method: string
+): SinonStub {
+  // force method to keyof T to allow stubbing private, protected, and methods otherwise not exposed in typings
+  return sandbox.stub(target, method as keyof T);
 }
 
 /**
@@ -61,9 +66,13 @@ export function stubMethod<T extends object>(sandbox: SinonSandbox, target: T, m
  * @param target The target object of the stubbing operation.
  * @param method The method name of the stub.
  */
-export function spyMethod<T extends object>(sandbox: SinonSandbox, target: T, method: string): SinonSpy {
-    // force method to keyof T to allow spying on private, protected, and methods otherwise not exposed in typings
-    return sandbox.spy(target, method as keyof T);
+export function spyMethod<T extends object>(
+  sandbox: SinonSandbox,
+  target: T,
+  method: string
+): SinonSpy {
+  // force method to keyof T to allow spying on private, protected, and methods otherwise not exposed in typings
+  return sandbox.spy(target, method as keyof T);
 }
 
 /**
@@ -81,13 +90,13 @@ export function spyMethod<T extends object>(sandbox: SinonSandbox, target: T, me
  * @param members Optional overrides of zero or more members of the object.
  */
 export function stubObject<T extends object>(
-    sandbox: SinonSandbox,
-    target: T,
-    members: OpenDictionary = { }
+  sandbox: SinonSandbox,
+  target: T,
+  members: OpenDictionary = {}
 ): StubbedType<T> {
-    return new Proxy(target, {
-        get: makeProxyGet(sandbox, members, false)
-    }) as StubbedType<T>;
+  return new Proxy(target, {
+    get: makeProxyGet(sandbox, members, false)
+  }) as StubbedType<T>;
 }
 
 /**
@@ -105,12 +114,15 @@ export function stubObject<T extends object>(
  * @param members Optional overrides of zero or more members of the object.
  */
 export function stubInterface<T extends object>(
-    sandbox: SinonSandbox,
-    members: OpenDictionary = { }
+  sandbox: SinonSandbox,
+  members: OpenDictionary = {}
 ): StubbedType<T> {
-    return new Proxy({ }, {
-        get: makeProxyGet(sandbox, members, true)
-    }) as StubbedType<T>;
+  return new Proxy(
+    {},
+    {
+      get: makeProxyGet(sandbox, members, true)
+    }
+  ) as StubbedType<T>;
 }
 
 /**
@@ -122,16 +134,17 @@ export function stubInterface<T extends object>(
  * @param members Optional overrides of zero or more members of the object.
  */
 export function stubCallable<T extends object>(
-    sandbox: SinonSandbox,
-    members: OpenDictionary = { },
-    fake: AnyFunction = () => { }
+  sandbox: SinonSandbox,
+  members: OpenDictionary = {},
+  fake: OpenFunction = () => {}
 ): StubbedCallableType<T> {
-    return new Proxy(sandbox.stub().callsFake(fake), {
-        get: makeProxyGet(sandbox, members, false),
-        apply: (target: AnyFunction, thisArg: any, argumentsList: any[]) => { // tslint:disable-line:no-any
-            return target.apply(thisArg, argumentsList);
-        }
-    }) as StubbedCallableType<T>;
+  return new Proxy(sandbox.stub().callsFake(fake), {
+    get: makeProxyGet(sandbox, members, false),
+    apply: (target: OpenFunction, thisArg: any, argumentsList: any[]) => {
+      // tslint:disable-line:no-any
+      return target.apply(thisArg, argumentsList);
+    }
+  }) as StubbedCallableType<T>;
 }
 
 /**
@@ -142,38 +155,42 @@ export function stubCallable<T extends object>(
  * a {@link stubObject} call into a function or constructor requiring the original class type `T`.
  */
 export function fromStub<T extends object>(stubbed: StubbedType<T>): T {
-    // Since `stubObject` produces a `Proxy` of `T` backed by a real instance of `T`, it's safe to simply achieve this
-    // with a type assertion.  Since much of the point of this library is to help avoid the need for type assertions,
-    // prefer the use of using `fromStub` over the direct assertion.
-    return stubbed as T;
+  // Since `stubObject` produces a `Proxy` of `T` backed by a real instance of `T`, it's safe to simply achieve this
+  // with a type assertion.  Since much of the point of this library is to help avoid the need for type assertions,
+  // prefer the use of using `fromStub` over the direct assertion.
+  return stubbed as T;
 }
 
 // Internal implementation for the generated proxy getters.
-const makeProxyGet = (sandbox: SinonSandbox, members: OpenDictionary, stubMissing: boolean) => {
-    const cache: OpenDictionary = { };
-    return (target: OpenDictionary, name: string) => {
-        const stubMemberFn = (fn: AnyFunction) => {
-            if (Object.keys(fn).length > 0) {
-                return stubCallable(sandbox, fn, fn);
-            }
-            return sandbox.stub().callsFake(fn);
-        };
-        if (cache[name] != null) {
-            return cache[name];
-        }
-        if (members[name] != null) {
-            if (isFunction(members[name])) {
-                cache[name] = stubMemberFn(members[name]);
-            } else {
-                cache[name] = members[name];
-            }
-        } else if (isFunction(target[name])) {
-            cache[name] = stubMemberFn(target[name]);
-        } else if (target[name] == null && stubMissing) {
-            cache[name] = sandbox.stub();
-        } else {
-            cache[name] = target[name];
-        }
-        return cache[name];
+const makeProxyGet = (
+  sandbox: SinonSandbox,
+  members: OpenDictionary,
+  stubMissing: boolean
+) => {
+  const cache: OpenDictionary = {};
+  return (target: OpenDictionary, name: string) => {
+    const stubMemberFn = (fn: OpenFunction) => {
+      if (Object.keys(fn).length > 0) {
+        return stubCallable(sandbox, fn, fn);
+      }
+      return sandbox.stub().callsFake(fn);
     };
+    if (cache[name] != null) {
+      return cache[name];
+    }
+    if (members[name] != null) {
+      if (isFunction(members[name])) {
+        cache[name] = stubMemberFn(members[name]);
+      } else {
+        cache[name] = members[name];
+      }
+    } else if (isFunction(target[name])) {
+      cache[name] = stubMemberFn(target[name]);
+    } else if (target[name] == null && stubMissing) {
+      cache[name] = sandbox.stub();
+    } else {
+      cache[name] = target[name];
+    }
+    return cache[name];
+  };
 };
