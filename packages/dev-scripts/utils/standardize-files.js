a5e7e38960b35d19a83bb6b55d4df46ba59daaa8
Thomas <t.dvornik@salesforce.com>
2018-11-13 10:47:06 -0700

    fix: add tsconfig to test, and lint/compile test

--- packages/dev-scripts/utils/standardize-files.js
+++ packages/dev-scripts/utils/standardize-files.js
@@ -6,20 +6,28 @@
  */
 
 const { join } = require('path');
-const { accessSync, unlinkSync, writeFileSync } = require('fs');
+const { accessSync, writeFileSync } = require('fs');
 const log = require('./log');
+const exists = require('./exists');
 const SfdxDevConfig = require('./sfdx-dev-config');
 const PackageJson = require('./package-json');
 
-const fileExist = path => {
-  try {
-    accessSync(path);
-    return true;
-  } catch (err) {}
-  return false;
+const addFile = (filePath, strict, config) => {
+  const fileExists = exists(filePath);
+
+  // If it is strict, the user shouldn't be overriding anything,
+  // so write the file again.
+  if (strict || (!strict && !fileExists)) {
+    let contents = JSON.stringify(config, null, 2) + '\n';
+    if (strict) {
+      contents = `// Generated - Do not modify. Controlled by @salesforce/dev-scripts\n${contents}`;
+    }
+    writeFileSync(filePath, contents);
+    return filePath;
+  }
 };
 
-module.exports = (packageRoot = require('./package-path')) => {
+module.exports = (packageRoot = require('./package-path'), inLernaProject) => {
   const config = new SfdxDevConfig(packageRoot);
   let isLernaProject = false;
 
@@ -41,7 +49,8 @@ module.exports = (packageRoot = require('./package-path')) => {
 
   if (scriptList.length > 0) {
     const nycPath = join(packageRoot, '.nycrc');
-    if (scriptList.includes('test') && !fileExist(nycPath)) {
+    // nyc file
+    if (scriptList.includes('test') && !exists(nycPath)) {
       const nyc = {
         nyc: {
           extends: '@salesforce/dev-config/nyc'
@@ -52,27 +61,58 @@ module.exports = (packageRoot = require('./package-path')) => {
       added.push(nycPath);
     }
 
+    // tslint files
     if (scriptList.includes('lint')) {
       const lintConfig = config.get('lint') || {};
-      const strict = lintConfig.strict;
-      const tslintPath = join(packageRoot, 'tslint.json');
-      const exists = fileExist(tslintPath);
+      const strict = config.get('strict') || lintConfig.strict;
 
-      if (strict) {
+      const postfix = strict ? '-strict' : '';
+      const addLintFile = (baseDir, baseFile) => {
+        const tslintPath = join(baseDir, 'tslint.json');
         const tslint = {
-          extends: '@salesforce/dev-config/tslint-strict'
+          extends: `@salesforce/dev-config/${baseFile}${postfix}`
         };
-        const tslintJson = JSON.stringify(tslint, null, 2) + '\n';
-        writeFileSync(tslintPath, tslintJson);
-        added.push(tslintPath);
-      } else if (!strict && !exists) {
-        const tslint = {
-          extends: '@salesforce/dev-config/tslint'
-        };
-        const tslintJson = JSON.stringify(tslint, null, 2);
-        writeFileSync(tslintPath, tslintJson);
-        added.push(tslintPath);
-      }
+        const file = addFile(tslintPath, strict, tslint);
+        if (file) {
+          added.push(tslintPath);
+        }
+      };
+      addLintFile(packageRoot, 'tslint');
+      addLintFile(join(packageRoot, 'test'), 'tslint-test');
+    }
+
+    // tsconfig files
+    if (scriptList.includes('compile')) {
+      const compileConfig = config.get('compile') || {};
+      const strict = config.get('strict') || compileConfig.strict;
+
+      const prefix = inLernaProject ? '../../' : './';
+      const postfix = strict ? '-strict' : '';
+
+      const addConfigFile = (baseDir, config) => {
+        const tsconfigPath = join(baseDir, 'tsconfig.json');
+        const file = addFile(tsconfigPath, strict, config);
+        if (file) {
+          added.push(tsconfigPath);
+        }
+      };
+      // FIXME: Temporary fix until github.com/Microsoft/TypeScript/issues/18865 is fixed.
+      // TODO: Change to @salesforce/devconfig/tsconfig-strict when above is resolved.
+      addConfigFile(packageRoot, {
+        extends: `${prefix}node_modules/@salesforce/dev-config/tsconfig${postfix}`,
+        // This has to live in this file until there is a way to specify a base
+        // TODO Update when https://github.com/Microsoft/TypeScript/issues/25430 is fixed
+        compilerOptions: {
+          outDir: 'lib'
+        },
+        include: ['./src/**/*.ts']
+      });
+      addConfigFile(join(packageRoot, 'test'), {
+        extends: `${prefix}../node_modules/@salesforce/dev-config/tsconfig-test${postfix}`,
+        // This has to live in this file until there is a way to specify a base
+        // TODO Update when https://github.com/Microsoft/TypeScript/issues/25430 is fixed
+        include: ['./**/*.ts']
+      });
     }
   }
   if (added.length > 0) {
