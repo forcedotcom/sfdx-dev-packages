981a71beaa7a782c572b2a8188a3bb3ca466293c
Thomas <t.dvornik@salesforce.com>
2020-07-09 15:13:22 -0700

    chore: remove remaining tslint

--- packages/eslint-config-salesforce-typescript/.eslintrc.js
+++ packages/eslint-config-salesforce-typescript/.eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:prettier/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
