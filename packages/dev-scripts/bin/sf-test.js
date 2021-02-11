a906fb8da95e194a32ed768f84d6e9c7cbb6e9c6
Mike Donnalley <mdonnalley@salesforce.com>
2021-02-11 15:34:47 -0700

    fix: testing and linting on windows

--- packages/dev-scripts/bin/sf-test.js
+++ packages/dev-scripts/bin/sf-test.js
@@ -7,17 +7,17 @@
  */
 
 const shell = require('../utils/shelljs');
-
 const packageRoot = require('../utils/package-path');
 const { resolveConfig } = require('../utils/sf-config');
 
 const nyc = require.resolve('nyc/bin/nyc');
+const mocha = require.resolve('mocha/bin/mocha');
 
 const config = resolveConfig(packageRoot);
 const testConfig = config.test || {};
 const includes = testConfig.testsPath || '**/*.test.ts';
 
-const command = `${nyc} mocha "${includes}"`;
+const command = `node ${nyc} ${mocha} "${includes}"`;
 
 try {
   shell.exec(command, {
